41abb1b11000d6d1a0fea1e0ca50de3f9a0f1729
chenbaozhong <804615539@qq.com>
2020-09-17 09:46:38 +0800

    Test分支创建一个TestTest类，添加一个Test()方法

---
+++ src/main/java/com/atguigu/thymeleaf/demo/TestTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.thymeleaf.demo;
+
+/**
+ * @author Baozhong Chen
+ * @version 1.0
+ * @date 2020/9/17 9:44
+ */
+public class TestTest {
+    public void Test(){
+        System.out.println("测试方法");
+    }
+}
