522297d42b96aca187d48b8fcadd2d20eccbf720
chenbaozhong <804615539@qq.com>
2020-09-17 09:30:41 +0800

    dev1.0.0分支创建一个方法addTest()方法

---
+++ src/main/java/com/atguigu/thymeleaf/demo/DevTest01.java
@@ -0,0 +1,17 @@
+package com.atguigu.thymeleaf.demo;
+
+/**
+ * @author Baozhong Chen
+ * @version 1.0
+ * @date 2020/9/16 9:26
+ */
+public class DevTest01 {
+
+    /**
+     * 添加购物车
+     *
+     */
+    public void addCart(){
+        System.out.println("9月16日=======添加一个购物车");
+    }
+}
